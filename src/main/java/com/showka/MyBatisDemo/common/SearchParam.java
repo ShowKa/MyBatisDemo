a21cf4373c8a87d5e606edeedce5a47d2730a9a3
John Doe <johndoe@example.com>
2017-07-31 01:47:22 +0900

    add search mode & refactor

--- src/main/java/com/showka/MyBatisDemo/common/SearchParam.java
+++ src/main/java/com/showka/MyBatisDemo/common/SearchParam.java
@@ -4,13 +4,26 @@ import java.util.HashMap;
 
 import com.showka.MyBatisDemo.system.exception.SystemException;
 
+import lombok.Getter;
+
 /**
  * 検査パラメータ
  * 
  * @author ShowKa
  *
  */
+@Getter
 public class SearchParam extends HashMap<String, String> {
+
+	/**
+	 * 検索タイプ
+	 * 
+	 * @author ShowKa
+	 *
+	 */
+	private enum SearchMode {
+		Screen, Report
+	}
 
 	/**
 	 * SID.
@@ -23,13 +36,48 @@ public class SearchParam extends HashMap<String, String> {
 	private static final String anyPatter = "%";
 
 	/**
+	 * 取得制限
+	 */
+	private SearchMode mode;
+
+	/**
+	 * 画面検索モード
+	 */
+	public void setScreenMode() {
+		mode = SearchMode.Screen;
+	}
+
+	/**
+	 * 画面モード判定
+	 * 
+	 * @return 画面モードならtrue
+	 */
+	public boolean isScreenMode() {
+		return mode == SearchMode.Screen;
+	}
+
+	/**
+	 * 帳票検索モード
+	 */
+	public void setReportMode() {
+		mode = SearchMode.Report;
+	}
+
+	/**
+	 * 帳票モード判定
+	 * 
+	 * @return 帳票モードならtrue
+	 */
+	public boolean isReportMode() {
+		return mode == SearchMode.Report;
+	}
+
+	/**
 	 * パラメータ設定
 	 */
 	@Override
 	public String put(String key, String value) {
-		if (value == null) {
-			throw new SystemException("検索パラメータにnullを設定してはいけません。");
-		}
+		checkValueStrictly(value);
 		return super.put(key, value);
 	}
 
@@ -43,9 +91,7 @@ public class SearchParam extends HashMap<String, String> {
 	 * @return %value%
 	 */
 	public String putForPartialMatch(String key, String value) {
-		if (value == null) {
-			throw new SystemException("検索パラメータにnullを設定してはいけません。");
-		}
+		checkValueStrictly(value);
 		return put(key, anyPatter + value + anyPatter);
 	}
 
@@ -59,9 +105,7 @@ public class SearchParam extends HashMap<String, String> {
 	 * @return value%
 	 */
 	public String putForPrefixMatch(String key, String value) {
-		if (value == null) {
-			throw new SystemException("検索パラメータにnullを設定してはいけません。");
-		}
+		checkValueStrictly(value);
 		return put(key, value + anyPatter);
 	}
 
@@ -75,9 +119,20 @@ public class SearchParam extends HashMap<String, String> {
 	 * @return %value
 	 */
 	public String putForBackwardMatch(String key, String value) {
+		checkValueStrictly(value);
+		return put(key, anyPatter + value);
+	}
+
+	/**
+	 * valueチェック
+	 * 
+	 * <pre>
+	 * nullのパラメータを渡したらエラー
+	 * </pre>
+	 */
+	private void checkValueStrictly(String value) {
 		if (value == null) {
 			throw new SystemException("検索パラメータにnullを設定してはいけません。");
 		}
-		return put(key, anyPatter + value);
 	}
 }
