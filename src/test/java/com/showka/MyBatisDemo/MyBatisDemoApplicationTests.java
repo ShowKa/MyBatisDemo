1908d756c1822a992d55d9840e5fa592bc2f8e2b
John Doe <johndoe@example.com>
2017-07-31 19:17:31 +0900

    add framework.properties

--- src/test/java/com/showka/MyBatisDemo/MyBatisDemoApplicationTests.java
+++ src/test/java/com/showka/MyBatisDemo/MyBatisDemoApplicationTests.java
@@ -3,10 +3,13 @@ package com.showka.MyBatisDemo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@PropertySources({ @PropertySource("classpath:framework.properties") })
 public class MyBatisDemoApplicationTests {
 
 	@Test
